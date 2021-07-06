5afbee1e1cb556d11562390fa843749a569b2e37
Nahomi Alcivar <85972823+NahomiAlcivar14@users.noreply.github.com>
2021-07-06 09:51:59 -0500

    Update Operaciones con matrices.cpp

--- Operaciones con matrices.cpp
+++ Operaciones con matrices.cpp
@@ -78,3 +78,16 @@ int main()
 
     return 0;
 }
+//NAHOMI ALCIVAR//
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+void ingresar(float matriz[5][5]){
+  for(int i=0;i<=6;i++){
+    for(int j=0;j<=6;j++){
+      matriz[i][j]=rand()%(89-2+1)+2;
+    }
+  }
+}
