43c67d45e7ba5752951cd9b6a94a1df88fd7bfee
Nahomi Alcivar <85972823+NahomiAlcivar14@users.noreply.github.com>
2021-07-06 10:26:17 -0500

    Update Operaciones con matrices.cpp
    Parte 1 Nahomi: mostrar por pantalla.
    Parte 2 Jared: Realizar el algoritmo para obtener el producto de dos matrices.

--- Operaciones con matrices.cpp
+++ Operaciones con matrices.cpp
@@ -91,3 +91,17 @@ void ingresar(float matriz[5][5]){
     }
   }
 }
+//JARED BERNAL// 
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+void producto(float m1[5][5], float m2[5][5], float r[5][5]){
+    for(int i=0;i<5;i++){ 
+      for(int j=0;j<5;j++){
+          for(int k=0;k<5;k++){
+              r[i][j]=r[i][j]+(m1[i][k]*m2[k][j]);
+          }
+      }
+  }
+}
